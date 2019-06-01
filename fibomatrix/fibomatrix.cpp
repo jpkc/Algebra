ac82f69024f0e057dce440f9f44a50c7675a2062
João Pedro Kerr Catunda <jpkc_deb@DebianCortex.Tensores>
2019-05-31 21:45:35 -0300

    Making it solve any Fibonnacci like series

--- fibomatrix/fibomatrix.cpp
+++ fibomatrix/fibomatrix.cpp
@@ -1,83 +1,70 @@
 #include <iostream>
 
-#define VERBOSE 0
-
 using namespace std;
 
 typedef struct {
-	long long a, b, c, d;
+	 long long a, b, c, d;
 } M2x2;
 
-
 M2x2 MMult(const M2x2 &m1, const M2x2 &m2) {
-	M2x2 mult;
+	 M2x2 mult;
 
-	mult.a = m1.a*m2.a + m1.b*m2.c;
-	mult.b = m1.a*m2.b + m1.b*m2.d;
-	mult.c = m1.c*m2.a + m1.d*m2.c;
-	mult.d = m1.c*m2.b + m1.d*m2.d;
+	 mult.a = m1.a*m2.a + m1.b*m2.c;
+	 mult.b = m1.a*m2.b + m1.b*m2.d;
+	 mult.c = m1.c*m2.a + m1.d*m2.c;
+	 mult.d = m1.c*m2.b + m1.d*m2.d;
 
-	return mult;
+	 return mult;
 }
 
 void PrintM(M2x2 m) {
-	cout << m.a << '\t' << m.b << '\n' << m.c << '\t' << m.d << endl;
+	 cout << m.a << '\t' << m.b << '\n' << m.c << '\t' << m.d << endl;
 }
 
-long long F(M2x2 m, int n) {
-	long long f;
-	M2x2 fm, pm, id;
+M2x2 F(int n) {
+	 M2x2 fm, pm;
 
-	id.a = 1; id.b = 0; id.c = 0; id.d = 1;
-	fm = id;
-	pm = m;
+	 pm.a = 1; pm.b = 1; pm.c = 1; pm.d = 0; //fibomatrix
+	 fm.a = 1; fm.b = 0; fm.c = 0; fm.d = 1; //identity
 
-	for(int i = n; i > 0; i >>= 1) {
-		if(i % 2) {
-			fm = MMult(fm, pm);
-		}
-		pm = MMult(pm, pm);
-
-		if(VERBOSE) {
-			PrintM(fm);
-			cout << endl;
-			PrintM(pm);
-			cout << '\n' << fm.b << '\n'<< endl;
-		}
-	}
-	f = fm.b;
-	return f;
+	 for(int i = n; i > 0; i >>= 1) { //breaking n in powers of 2
+		  if(i % 2) {
+				fm = MMult(fm, pm);
+		  }
+		  pm = MMult(pm, pm);
+	 }
+	 return fm;
 }
 
-int IsLikeFibonacci(M2x2 matrix, int n) {
-	long long f;
-	switch (n) {
-		case 0:
-			f = matrix.d;
-			break;
-		case 1:
-			f = matrix.c;
-			break;
-		case 2:
-			f = matrix.a;
-			break;
-		default:
-			f = F(matrix, n);
-			break;
-	}
-	return f;
+int IsLikeFibonacci(long long a, long long b, int n) {
+	 long long f;
+	 M2x2 matrix;
+
+
+	 switch (n) {
+		  case 0:
+				f = a;
+				break;
+		  case 1:
+				f = b;
+				break;
+		  case 2:
+				f = a + b;
+				break;
+		  default:
+				matrix = F(n);
+				f = matrix.c*b + matrix.d*a;
+				break;
+	 }
+	 return f;
 }
 
 int main (int, char **) {
-	int n;
-	long long a, b;
-	M2x2 m;
+	 int n;
+	 long long a, b;
 
-	a = 0; b = 1; //F0 and f1. Used to feed matrix
-	cout << "O(log(n)) Fibbonacci calculator. Type n for desired Fn:" << endl;
-	cin >> n;
-	m.a = a + b;
-	m.b = m.c = b;
-	m.d = a;
-	cout << "Fn: " << IsLikeFibonacci(m, n) << endl;
+	 cout << "O(Log(n)) Fibonnacci like calculator. Please type F0 F1 and n for desired FN:" << endl;
+
+	  cin >> a >> b >> n;
+	  cout << "f" << n << ": " << IsLikeFibonacci(a, b, n) << endl;
 }
